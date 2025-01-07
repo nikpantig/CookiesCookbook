5090ea54de6f67b8da20c52310ab9b75a674eba7
Nikko Pantig <npantig@servingintel.com>
2025-01-07 10:09:45 +0800

    Initial upload of CookiesCookbook project

---
+++ App/IRecipesUserInteraction.cs
@@ -0,0 +1,14 @@
+﻿using CookiesCookbook.Recipes.Ingredients;
+using CookiesCookbook.Recipes;
+
+namespace CookiesCookbook.App
+{
+    public interface IRecipesUserInteraction
+    {
+        void ShowMessage(string message);
+        void Exit();
+        void PrintExistingRecipes(IEnumerable<Recipe> allRecipes);
+        void PromptToCreateRecipe();
+        IEnumerable<Ingredient> ReadIngredientsFromUser();
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using CookiesCookbook.App;
+using CookiesCookbook.DataAccess;
+using CookiesCookbook.FileAccess;
+using CookiesCookbook.Recipes;
+using CookiesCookbook.Recipes.Ingredients;
+using System.Text.Json;
+
+const FileFormat Format = FileFormat.Txt;
+
+IStringsRepository stringsRepository = Format == FileFormat.Json ?
+    new StringsJsonRepository() :
+    new StringsTextualRepository();
+
+const string FileName = "recipes";
+var fileMetadata = new FileMetadata(FileName, Format);
+
+var ingredientRegister = new IngredientsRegister();
+
+var cookiesRecipeApp = new CookiesRecipeApp(
+    new RecipesRepository(
+        stringsRepository,
+        ingredientRegister),
+    new RecipesConsoleUserInteraction(
+        ingredientRegister));
+
+cookiesRecipeApp.Run(fileMetadata.ToPath());
+
+
+
+
+
+
+
+
+
